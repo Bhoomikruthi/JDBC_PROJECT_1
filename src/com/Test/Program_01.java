5a4f68980ca611df4ed1ca622d6f1df5630d8de2
Bhoomika K N <158167852+Bhoomikruthi@users.noreply.github.com>
2025-05-06 09:31:54 +0530

    Update Program_01.java

--- src/com/Test/Program_01.java
+++ src/com/Test/Program_01.java
@@ -1,10 +1,14 @@
 package com.Test;
-
+import java.util.Scanner;
 public class Program_01 {
 
 	public static void main(String[] args) {
-
+		Scanner sc = new Scanner(System.in);
 		System.out.println("Hello World!...");
+		System.out.println("Enter a number");
+		int n = sc.nextInt();
+		int Square = n*n;
+		System.out.println("Square of the number"+n+"is"+Square);
 	}
 
 }
